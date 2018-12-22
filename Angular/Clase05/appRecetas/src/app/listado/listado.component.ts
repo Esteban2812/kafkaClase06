08314b7c5a77722cdd26b746dad02c35b134ac5b
alumno <alumno@admins-MacBook-Pro.local>
2018-12-21 20:23:04 -0500

    Rutas I

--- Angular/Clase05/appRecetas/src/app/listado/listado.component.ts
+++ Angular/Clase05/appRecetas/src/app/listado/listado.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { IReceta } from '../receta.interface';
 import { RecetaService } from '../receta.service';
 import { LogService } from '../log.service';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-listado',
@@ -14,11 +15,15 @@ export class ListadoComponent implements OnInit {
 
 	recetas: Array<IReceta>
 
-	constructor(private recetaService: RecetaService/*, private logService: LogService*/) { }
+	constructor(private recetaService: RecetaService, private router: Router/*, private logService: LogService*/) { }
 
 	ngOnInit() {
 		this.recetas = this.recetaService.listarRecetas()
 		//this.logService.escribir("Lista recetas", "info")
+	}
+
+	nuevaReceta() {
+		this.router.navigate(["recetas", "nuevo"])
 	}
 
 }
